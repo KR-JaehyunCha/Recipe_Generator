fc2a71b532361441a7ca772243159dd2d8e702d2
Jaehyun Cha <83200506+KR-JaehyunCha@users.noreply.github.com>
2025-03-08 16:17:21 -0800

    Greeting Message Added

--- FrontEnd/src/components/ChatWindow.jsx
+++ FrontEnd/src/components/ChatWindow.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import GreetingMessage from './GreetingMessage';
 import '../styles/ChatWindow.css';
 
 const ChatWindow = ({ messages }) => {
   return (
     <div className="chat-window">
+      {/* 채팅 시작 전 인사말 애니메이션 */}
+      <GreetingMessage />
+
       {messages.map((msg, index) => (
         <div
           key={index}
